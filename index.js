4c5bb9ffd2fd7b8241569b59d8401ec184f0d772
arifprog <arifprog1@gmail.com>
2025-01-11 17:35:35 +0600

    To create add task with new word

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let inputBox = document.querySelector ('#inputBox');
 let addBtn = document.querySelector('#addBtn');
+let listbox = document.querySelector('p');
 
 addBtn.addEventListener('click',(event)=>{
     event.preventDefault();
+    
     if(inputBox.value == ''){
         alert('Please add a new task.');
         return;
     }
+
+    let option = new Option(inputBox.value,inputBox.value);
+    
+    let task = listbox.appendChild(option);
+    inputBox.value = '';
+
 })

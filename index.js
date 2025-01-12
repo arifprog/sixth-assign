de2a442ed6f24de48158750645c79b3d8d3f2afe
arifprog <arifprog1@gmail.com>
2025-01-13 02:43:52 +0600

    To create Edit button for upgrade

--- index.js
+++ index.js
@@ -14,8 +14,25 @@ addBtn.addEventListener('click',(event)=>{
 
     let listItem = document.createElement('li');
     listItem.textContent = inputBox.value;
-    listItem.className = 'bg-gray-400 w-[300px] md:w-[900px] ml-10 px-4 py-2 rounded ';
+    listItem.className = 'bg-gray-400 w-[300px] md:w-[900px] ml-20 px-8 py- rounded  ';
     
     taskList.appendChild(listItem);
     inputBox.value = '';
+
+    let editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.className = 'ml-[900px] bg-green-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded ';
+    listItem.appendChild(editButton);
+    editButton.style.paddingTop = '12px';
+    
+
+    editButton.addEventListener('click', function() {
+    let newTask = prompt('Edit your task:', listItem.textContent);
+    if (newTask !== null && newTask !== '') {
+        listItem.textContent = newTask;
+        listItem.appendChild(editButton); 
+    }
+});
+
+    
 })

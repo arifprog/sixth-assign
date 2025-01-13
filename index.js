b0b126c8efef563547198f67e52865eddfef31bf
arifprog <arifprog1@gmail.com>
2025-01-13 22:55:14 +0600

    Inline all button and list

--- index.js
+++ index.js
@@ -11,28 +11,30 @@ addBtn.addEventListener('click',(event)=>{
         alert('Please add a new task.');
         return;
     }
-
+    
     let listItem = document.createElement('li');
     listItem.textContent = inputBox.value;
-    listItem.className = 'bg-gray-400 w-[800px] ml-20 my-2 p- px-8 rounded shadow-md flex items-center row';
+    listItem.className = 'bg-gray-400 w-[800px] mx-auto my- p- px-8 rounded shadow-md flex h-[50px] relative';
     
     taskList.appendChild(listItem);
     inputBox.value = '';
 
+    
     let buttonContainer = document.createElement('div');
-    buttonContainer.className = 'flex space-x-2';
+    buttonContainer.className = 'flex space-x-2 absolute top-0';
     listItem.appendChild(buttonContainer);
+    
 
     let editButton = document.createElement('button');
     editButton.textContent = '';
-    editButton.className = 'ml-[900px] bg-green-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded ';
+    editButton.className = 'ml-[900px] bg-green-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded  ';
     buttonContainer.appendChild(editButton);
     editButton.innerHTML = '<i class="fas fa-edit"></i>';
     
     
     let deleteButton = document.createElement('button');
     deleteButton.textContent = '';
-    deleteButton.className = 'ml-[900px] bg-red-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded ';
+    deleteButton.className = 'ml-[900px] bg-red-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded  ';
     buttonContainer.appendChild(deleteButton);
     deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
     

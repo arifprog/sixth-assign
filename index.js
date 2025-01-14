f636b969a4919ba81efde2b03c1a0c3dd983552a
arifprog <arifprog1@gmail.com>
2025-01-14 19:58:17 +0600

    Create effect after click edit btn

--- index.js
+++ index.js
@@ -14,27 +14,31 @@ addBtn.addEventListener('click',(event)=>{
     
     let listItem = document.createElement('li');
     listItem.textContent = inputBox.value;
-    listItem.className = 'bg-gray-400 w-[800px] mx-auto my- p- px-8 rounded shadow-md flex h-[50px] relative';
+    listItem.className = 'bg-gray-400 w-[800px] ml-[180px] my- p- px-8 rounded-xl shadow-md flex py-3 relative pt-3';
     
     taskList.appendChild(listItem);
     inputBox.value = '';
 
     
     let buttonContainer = document.createElement('div');
-    buttonContainer.className = 'flex space-x-2 absolute top-0';
+    buttonContainer.className = 'flex space-x-2 absolute top-0 ';
     listItem.appendChild(buttonContainer);
     
 
     let editButton = document.createElement('button');
     editButton.textContent = '';
-    editButton.className = 'ml-[900px] bg-green-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded  ';
+    editButton.className = 'ml-[850px] bg-green-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded-xl shadow-md ';
     buttonContainer.appendChild(editButton);
     editButton.innerHTML = '<i class="fas fa-edit"></i>';
-    
+
+    textColor = editButton.addEventListener('click', function() {
+    listItem.style.color = 'white';
+    listItem.style.backgroundColor = 'cadetblue';
+    });
     
     let deleteButton = document.createElement('button');
     deleteButton.textContent = '';
-    deleteButton.className = 'ml-[900px] bg-red-500 hover:bg-pink-700 text-white font-bold w-[150px] py-3 rounded  ';
+    deleteButton.className = 'ml-[850px] bg-red-500 hover:bg-purple-700 text-white font-bold w-[150px] py-3 rounded-xl shadow-md  ';
     buttonContainer.appendChild(deleteButton);
     deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
     

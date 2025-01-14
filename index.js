e07757237d918a70caa66aa2612963d2075b6f71
arifprog <arifprog1@gmail.com>
2025-01-15 02:47:11 +0600

    complete with responsive

--- index.js
+++ index.js
@@ -14,21 +14,20 @@ addBtn.addEventListener('click',(event)=>{
     
     let listItem = document.createElement('li');
     listItem.textContent = inputBox.value;
-    listItem.className = 'bg-gray-400 w-[800px] ml-[270px] my- p- px-8 rounded-xl shadow-md flex py-3 relative pt-3';
+    listItem.className = 'bg-gray-400 w-[250px] md:w-[800px] ml-[5px] md:ml-[260px] my- p- px-8 rounded-xl shadow-md flex py-3 relative pt-3';
     
     taskList.appendChild(listItem);
     inputBox.value = '';
 
     
     let buttonContainer = document.createElement('div');
-    buttonContainer.className = 'flex space-x-2 absolute top-0 ';
+    buttonContainer.className = 'flex space-x-1 md:space-x-2 absolute top-0  ';
     listItem.appendChild(buttonContainer);
     
     
-
     let editButton = document.createElement('button');
     editButton.textContent = '';
-    editButton.className = 'ml-[800px] bg-green-500 hover:bg-lime-600 text-white font-bold w-[50px] py-3 rounded-xl shadow-md ';
+    editButton.className = 'ml-[223px] md:ml-[800px] bg-green-500 hover:bg-lime-600 text-white font-bold w-[15px] md:w-[50px] py-3 rounded-xl shadow-md ';
     buttonContainer.appendChild(editButton);
     editButton.innerHTML = '<i class="fas fa-edit"></i>';
 
@@ -40,7 +39,7 @@ addBtn.addEventListener('click',(event)=>{
     
     let doneButton = document.createElement('button');
     doneButton.textContent = '';
-    doneButton.className = 'ml-[800px] bg-orange-500 hover:bg-amber-700 text-white font-bold w-[50px] py-3 rounded-xl shadow-md ';
+    doneButton.className = 'ml-[10px] md:ml-[800px] bg-orange-500 hover:bg-amber-700 text-white font-bold w-[15px] md:w-[50px] py-3 rounded-xl shadow-md ';
     buttonContainer.appendChild(doneButton);
     doneButton.innerHTML = '<i class="fas fa-check"></i>';
 
@@ -52,7 +51,7 @@ addBtn.addEventListener('click',(event)=>{
     
     let deleteButton = document.createElement('button');
     deleteButton.textContent = '';
-    deleteButton.className = 'ml-[800px] bg-red-500 hover:bg-rose-700 text-white font-bold w-[50px] py-3 rounded-xl shadow-md  ';
+    deleteButton.className = 'ml-[10px] md:ml-[800px] bg-red-500 hover:bg-rose-700 text-white font-bold w-[15px] md:w-[50px] py-3 rounded-xl shadow-md  ';
     buttonContainer.appendChild(deleteButton);
     deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
@@ -73,4 +72,4 @@ deleteButton.addEventListener('click', function () {
     taskList.removeChild(listItem);
 });
     
-})
+});
